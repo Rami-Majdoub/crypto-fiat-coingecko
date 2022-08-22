eae9a7928103a11b9107206e244265c07ee8e550
rami-majdoub <rami.majdoub1@gmail.com>
2022-08-22 04:44:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+token symbol (above the price)
+
+
+
+get 1 token price in usd
+
+	node index.js --tokens="ETH"
+
+get multiple token prices in usd
+
+	node index.js --tokens="ETH, MATIC, BAT, BCT"
+
+1 BTC = ? ETH
+
+	node index.js --tokens="BTC:ETH"
+
---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+if(process.env.API_KEY == undefined){
+  console.log("Envirement variable API_KEY is required");
+  return;
+}
+
+const axios = require('axios');
+
+const { tokens } = require('minimist')(process.argv.slice(2));
+
+// https://coinmarketcap.com/api/documentation/v1/#operation/getV2ToolsPriceconversion
+getConversionRate = async (from, to = "USD") => {
+  let response;
+  try {
+    response = await axios.get(
+      `https://pro-api.coinmarketcap.com/v2/tools/price-conversion?convert=${to}&symbol=${from}&amount=1`,
+      {
+        headers: {
+          'X-CMC_PRO_API_KEY': process.env.API_KEY || 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+        },
+      }
+    );
+  } catch(ex) {
+    console.log(ex.message);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(`1 ${json.data[0].symbol.padEnd(10)} = ${json.data[0].quote[to].price} ${to}`);
+  }
+}
+
+(async () => {
+	tokens.split(", ").forEach(async currency => await getConversionRate(...currency.split(":")))
+})();
