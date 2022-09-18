c362aad7e09d73e1c89871cccfe8d62f9c9de31c
rami-majdoub <rami.majdoub1@gmail.com>
2022-09-18 22:55:00 +0100

    minor changes: take api key from arguments, ignore spaces in tokens list, delete default api key, reformat error message.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
+const { tokens, apiKey } = require('minimist')(process.argv.slice(2));
 
-if(process.env.API_KEY == undefined){
-  console.log("Envirement variable API_KEY is required");
+if(process.env.API_KEY == undefined && apiKey == undefined){
+  console.log("Argument apiKey is required (usage: --apiKey=\"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\" get your api key from https://pro.coinmarketcap.com/account)");
+  return;
+}
+
+if(tokens == undefined){
+  console.log("Argument tokens is required (usage: --tokens=\"ETH\")");
   return;
 }
 
 const axios = require('axios');
-
-const { tokens } = require('minimist')(process.argv.slice(2));
 
 // https://coinmarketcap.com/api/documentation/v1/#operation/getV2ToolsPriceconversion
 getConversionRate = async (from, to = "USD") => {
@@ -17,20 +21,20 @@ getConversionRate = async (from, to = "USD") => {
       `https://pro-api.coinmarketcap.com/v2/tools/price-conversion?convert=${to}&symbol=${from}&amount=1`,
       {
         headers: {
-          'X-CMC_PRO_API_KEY': process.env.API_KEY || 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+          'X-CMC_PRO_API_KEY': apiKey || process.env.API_KEY,
         },
       }
     );
   } catch(ex) {
-    console.log(`"${from}" => "${to}":  ${ex.message}`);
+    console.log(`1 ${from.padEnd(10)} = ${to}: error(${ex.message})`);
   }
   if (response) {
     // success
-    const json = response.data;
-    console.log(`1 ${json.data[0].symbol.padEnd(10)} = ${json.data[0].quote[to].price} ${to}`);
+    const [ result ] = response.data.data;
+    console.log(`1 ${result.symbol.padEnd(10)} = ${result.quote[to].price} ${to}`);
   }
 }
 
 (async () => {
-	tokens.split(", ").forEach(async currency => await getConversionRate(...currency.split(":")))
+	tokens.replaceAll(" ", "").split(",").forEach(async currency => await getConversionRate(...currency.split(":")))
 })();
